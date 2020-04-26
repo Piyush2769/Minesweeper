4222648294bdca124dc4cf46afdd4b9afa2a2620
piyushmaheswari <piyush.ext@grofers.com>
2020-04-26 11:19:38 +0530

    Added Generator class

--- app/src/main/java/com/piyushmaheswari/minesweeper/util/GameEngine.java
+++ app/src/main/java/com/piyushmaheswari/minesweeper/util/GameEngine.java
@@ -1,4 +1,23 @@
 package com.piyushmaheswari.minesweeper.util;
 
+import android.content.Context;
+
 public class GameEngine {
+    private static GameEngine instance;
+
+    public static final int BOMB_NUMBER = 10;
+    public static final int WIDTH = 10;
+    public static final int HEIGHT = 16;
+
+    private Context context;
+
+
+    public static GameEngine getInstance() {
+        if( instance == null ){
+            instance = new GameEngine();
+        }
+        return instance;
+    }
+
+    private GameEngine(){ }
 }
